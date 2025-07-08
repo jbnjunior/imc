a2075a83fc89d8a18db7fcf44cf0fe99a9f44f19
Seu Nome <seu@email.com>
2025-07-07 21:01:32 -0300

    ajuste nos intervalos de classificação do IMC conforme OMS

--- imc.py
+++ imc.py
@@ -9,13 +9,13 @@ print(f"Seu IMC é: {imc:.2f}")
 
 if imc < 18.5:
     print("Classificação: Baixo peso")
-elif 18.5 <= imc < 24.9:
+elif 18.5 <= imc < 25:
     print("Classificação: Peso adequado")
-elif 25.0 <= imc < 29.9:
+elif 25 <= imc < 30:
     print("Classificação: Sobrepeso")
-elif 30.0 <= imc < 34.9:
+elif 30 <= imc < 35:
     print("Classificação: Obesidade grau I")
-elif 35.0 <= imc < 39.9:
+elif 35 <= imc < 40:
     print("Classificação: Obesidade grau II")
 else:
     print("Classificação: Obesidade grau III")
